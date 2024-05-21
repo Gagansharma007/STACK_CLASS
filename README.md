c57c22ad2d76df7ee1aa865f75c50a91a69fd903
Gagan sharma <sharmagagan01234@gmail.com>
2024-05-21 12:15:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is Stack class made from vector.
---
+++ stack.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Stack{
+    private:
+    vector<int> st;
+    public: 
+    int d , e;
+    void pushData( int a );
+    int popData(){
+        if ( isEmpty() ) return -1;
+        int val = st[ st.size() - 1 ];
+        st.pop_back();
+        return val;
+    }
+    int top(){
+        if( isEmpty() ) return -1;
+        int val = st[st.size() - 1];
+        return val;
+    }
+    bool isEmpty(){
+        if( st.size() < 1 ) return true;
+        else return false;
+    }
+};
+void Stack :: pushData(int a ){
+    st.push_back(a);
+}
+int main(){
+    Stack s;
+    s.pushData(1);
+    s.pushData(2);
+    cout << "Top of Stack: " << s.top() << endl;
+    cout << "Removed element: " << s.popData() << endl;
+    cout << "Top of Stack: " << s.top() << endl;
+    cout << "Removed element: " << s.popData() << endl;
+    if( s.isEmpty() ) cout << "Empty" << endl;
+    return 0;
+}
